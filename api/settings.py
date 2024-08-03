96c69c98137169019923d06cef0ce5a21df53659
Claudinei <claudineimoreirax7l@gmail.com>
2024-08-02 23:53:27 -0300

    Correções no settings

--- api/settings.py
+++ api/settings.py
@@ -56,6 +56,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'api.urls'
 
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "http://localhost:3000",
+]
+
+CORS_ALLOW_ALL_ORIGINS = False
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
