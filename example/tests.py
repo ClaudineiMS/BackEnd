5e14c8d00e23341934977f02d8a4194a7f36b0cb
Claudinei <claudineimoreirax7l@gmail.com>
2024-08-03 21:20:59 -0300

    Testes automatizados

---
+++ example/tests.py
@@ -0,0 +1,27 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+
+class URLTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index_url(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)  # Verifica se o status é 200 OK
+
+    def test_fornecedores_url(self):
+        response = self.client.get(reverse('fornecedores'))
+        self.assertEqual(response.status_code, 200)  
+
+    def test_fornecedores_por_consumo_url(self):
+        response = self.client.get(reverse('fornecedores_por_consumo', args=[500]))
+        self.assertEqual(response.status_code, 200) 
+
+    def test_fornecedores_por_consumo_nao_atentido(self):
+        response = self.client.get(reverse('fornecedores_por_consumo', args=[100]))
+        self.assertEqual(response.status_code, 200) 
+
+    #Teste que falha
+    # def test_fornecedores_por_consumo_vazio(self):
+    #     response = self.client.get(reverse('fornecedores_por_consumo', args=[None]))
+    #     self.assertEqual(response.status_code, 200)  
